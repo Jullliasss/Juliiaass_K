5e055e20d61f1a8e91436d2bd559e5ef7f54a915
Jullliasss <julia11koroliova@gmail.com>
2022-03-14 23:57:39 +0200

    Comparing two numbers, which closely to 10

---
+++ src/task2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+
+public class task2 {
+    public static void main(String[] args) {
+
+        //enter first number from keyboard
+        double num1;
+        System.out.println("Input first number: ");
+
+        Scanner input1 = new Scanner(System.in);
+        num1 = input1.nextDouble();
+
+        //enter second number from keyboard
+        double num2;
+        System.out.println("Input second number: ");
+
+        Scanner input2 = new Scanner(System.in);
+        num2 = input2.nextDouble();
+
+        double num = 10;
+
+      // compare difference of absolute numbers, which closer to num (10)
+          System.out.println(abs((num1 - num)) < abs((num2 - num)) ? num1 : num2);
+
+    }
+}
