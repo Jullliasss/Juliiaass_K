f0b2e750318393824d3645ca6dd5ad7eff02b37c
Jullliasss <julia11koroliova@gmail.com>
2022-03-14 23:06:37 +0200

    Add explanation

--- src/Julia_QA.java
+++ src/Julia_QA.java
@@ -4,14 +4,15 @@ public class Julia_QA {
     public static void main(String[] args) {
 
        int num;
-        System.out.println("Input: ");
+        System.out.println("Input number: ");
 
         Scanner input = new Scanner(System.in);
        num = input.nextInt();
-
+       //checking the remainder for "0"
          if (num % 2 == 0)
-             System.out.println("Число четное");
+             System.out.println("Even number");
+
          else
-             System.out.println("Число нечетное");
+             System.out.println("Odd number");
     }
 }
